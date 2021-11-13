f00d69e7a2948a7fd450906dcd2692f34ee5b126
Anthony <ajsur1017@gmail.com>
2021-11-13 16:18:28 -0500

    errors fixed on frontend

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { RecoilRoot } from "recoil";
-import { QueryClient, QueryClientProvider } from "react-query";
-
-const queryClient = new QueryClient();
 
 ReactDOM.render(
-  <QueryClientProvider client={queryClient}>
-    <RecoilRoot>
-      <React.StrictMode>
-        <App />
-      </React.StrictMode>
-    </RecoilRoot>
-  </QueryClientProvider>,
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
   document.getElementById("root")
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
